32248b7a27cc357694920719f539444c6e95aab4
Ties Petersen <75524368+TiesPetersen@users.noreply.github.com>
2024-12-03 17:32:18 +0100

    Update Checkbox bug

--- components/Checkbox.js
+++ components/Checkbox.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Checkbox({text, checked, onChange, color}) {
   return (
     <div className='flex gap-3' key={color.id}>
-    <input onChange={onChange} checked={checked} className='h-6 w-6 border-2 border-zinc-100 appearance-none rounded hover:bg-zinc-600 checked:hover:bg-zinc-100 checked:bg-zinc-100' type='checkbox' name={color.id}/>
+    <input onChange={onChange} checked={checked} className='h-6 w-6 border-2 border-zinc-100 appearance-none rounded hover:bg-zinc-600 checked:hover:bg-zinc-100 checked:bg-zinc-100' type='checkbox' id={color.id} name={color.id}/>
     <label htmlFor={color.id}>{color.nameLoc}</label>
 </div>
   )
